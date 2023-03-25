f5bbcc0fc8088f95c0b518772c27ae06d039255b
Arzt <ooalexis.samaoo@gmail.com>
2023-03-24 18:08:45 -0600

    agregado "Pizzeria.java", metodo cuadrito
    se agrego pizzeria.java con algunas variables sets gets, se modifico
    el Ap para crear la pizzeria, se agrego a pizzeria metodos
    para mostrar en un cuadrado centrado alguna palabra.

--- Ap.java
+++ Ap.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
 public class Ap{
-	public static void main(String[] args) {
+	public static void main (String[] args){
+		Pizzeria pizzafactory = new Pizzeria("Pizza Factory", "Av. Lic. Enrique Ramírez Miguel, Las Américas", "PizzaFactory@Pizza.com", "07002200" , "445697234");
+		pizzafactory.mostrar();
+		do {
+            switch (menu()) {
+                case 11:
+                  
+                    break;
+                case 0:
+                    return;
+            }
+        } while (true);
+    }
 
-	}
+    public static int menu() {
+        Scanner datos = new Scanner(System.in);
+		System.out.println("1.- 0.-Salir");
+        int seleccion = datos.nextInt();
+        if (seleccion == 0) {
+            return seleccion;
+        }
+
+		return seleccion;
+    }
+	 
 }
