c21674fe1d882326b2c29b991d8b36b716a70691
Arzt <ooalexis.samaoo@gmail.com>
2023-03-29 09:05:12 -0600

    funciones cuadrito renombradas a cuadrito y movidas a nueva clase

---
+++ Cuadrado.java
@@ -0,0 +1,65 @@
+public class Cuadrado {
+    char[][] matriz;
+    //funciones para cuadrado, considerar que es magia.
+
+    public void imprimirCuadrado(int x,int y, String palabra){
+        matriz=cuadrado(x,y );
+        centrarEnXY(matriz,palabra,0,0);
+        imprimirCuadrado();
+    }
+
+    public void imprimirCuadradoDividido(int x,int y, String palabra, String palabra2){
+        matriz=cuadrado(x, y);
+        for (int i = 0; i+1 < matriz[0].length-1; i++) {
+            centrarEnXY(matriz,"|",0,i-((int)(matriz[0].length/2)-1));
+        }
+        centrarEnXY(matriz,palabra,-1,0);
+        centrarEnXY(matriz,palabra2,1,0);
+        imprimirCuadrado();
+    }
+
+
+    public void imprimirCuadrado(){
+        for (int y = 0; y < matriz[0].length; y++) {
+            for (int x = 0; x < matriz.length; x++) {
+                System.out.print(matriz[x][y]); 
+            }
+            System.out.println();
+        }
+    }
+
+    public char[][] centrarEnXY(char[][] matriz, String palabra,int posicion, int posicionY){
+        int center = matriz.length/2;
+        for (int y = 0; y < matriz[0].length; y++) {
+            for (int x = 0; x < matriz.length; x++) {
+                if (y == ((int)matriz[0].length/2)+posicionY && x >= (matriz.length/2 - palabra.length()/2)+((int)(center/2)*posicion) && x < (matriz.length/2 + palabra.length()/2 + palabra.length()%2)+((int)(center/2)*posicion)) {
+                    matriz[x][y] = palabra.charAt((x - matriz.length/2 + palabra.length() / 2)-((center/2)*posicion));
+                }
+            }
+        }
+        return matriz;
+    }
+
+    public char[][] cuadrado(int horizontal, int vertical){
+        char[][] matriz = new char[horizontal+1][vertical+1];
+        char borde;
+        char borde2;
+        for (int y = 0; y <= vertical; y++) {
+            for (int x = 0; x <= horizontal; x++) {
+                borde = '/';
+                borde2 = '|';
+                if(y==0 || y==vertical){
+                    matriz[x][y]=borde;
+                }else{
+                    if(x==0 || x==horizontal||x==1|| x==horizontal-1){
+                        matriz[x][y]=borde2;
+                    }else{
+                        matriz[x][y]=' ';
+                    }
+                }
+            }
+        }
+        return matriz;
+    }
+    //fin de funciones de cuadrito
+}
--- Pizzeria.java
+++ Pizzeria.java
@@ -1,12 +1,11 @@
 import java.util.*;
-public class Pizzeria{
+public class Pizzeria extends Cuadrado {
     private String nombre;
     private String domicilio;
     private String correo;
     private String horario;
     private String telefono;
     private String rfc;
-    char[][] matriz;
 
 
     public Pizzeria() {
@@ -25,21 +24,21 @@ public class Pizzeria{
         
         //primero decir de que tamano sera la matriz x,y, 
         //luego darle la palabra a imprimir en cuadrito
-        imprimirCuadrito(50, 4 ,nombre);
+        imprimirCuadrado(50, 4 ,nombre);
 
         //para multiples palabras en un mismo cuadrito 
-        //asignar el tamano que sera la matriz, luego introducir la matriz
-        // luego usar el centrarEnXY para escojer donde iran las palabras.
-        matriz=cuadrito(50, 6 );
+        //asignar el tamano que sera la matriz, luego usar centrarEnXY
+        //pasarle esa matriz , la palabra y la posicion X,Y, 0,0 es el centro 
+        matriz=cuadrado(50, 6 );
         centrarEnXY(matriz,domicilio,0,-1);
         centrarEnXY(matriz,"Correo: "+correo,0,0);
         centrarEnXY(matriz,"horario: "+ horario,0,1);
-        imprimirCuadrito();
+        imprimirCuadrado();
 
         //para imprimir 2 palabras en izquierda y derecha centrados con division al medio'
-        imprimirCuadritoDividido(50, 4,"1.-Orden","2.-Menu");
-        imprimirCuadritoDividido(50, 4,"3.-Inventario","4.-Compra");
-        imprimirCuadritoDividido(50, 4,"5.-Proveedores","6.-Clientes");
+        imprimirCuadradoDividido(50, 4,"1.-Orden","2.-Menu");
+        imprimirCuadradoDividido(50, 4,"3.-Inventario","4.-Compra");
+        imprimirCuadradoDividido(50, 4,"5.-Proveedores","6.-Clientes");
         //
 
     }
@@ -98,66 +97,4 @@ public class Pizzeria{
         return rfc;
     }
 
-    //funciones para cuadrito, considerar que es magia.
-
-    public void imprimirCuadrito(int x,int y, String palabra){
-        matriz=cuadrito(x,y );
-        centrarEnXY(matriz,palabra,0,0);
-        imprimirCuadrito();
-    }
-
-    public void imprimirCuadritoDividido(int x,int y, String palabra, String palabra2){
-        matriz=cuadrito(x, y);
-        for (int i = 0; i+1 < matriz[0].length-1; i++) {
-            centrarEnXY(matriz,"|",0,i-((int)(matriz[0].length/2)-1));
-        }
-        centrarEnXY(matriz,palabra,-1,0);
-        centrarEnXY(matriz,palabra2,1,0);
-        imprimirCuadrito();
-    }
-
-
-    public void imprimirCuadrito(){
-        for (int y = 0; y < matriz[0].length; y++) {
-            for (int x = 0; x < matriz.length; x++) {
-                System.out.print(matriz[x][y]); 
-            }
-            System.out.println();
-        }
-    }
-
-    public char[][] centrarEnXY(char[][] matriz, String palabra,int posicion, int posicionY){
-        int center = matriz.length/2;
-        for (int y = 0; y < matriz[0].length; y++) {
-            for (int x = 0; x < matriz.length; x++) {
-                if (y == ((int)matriz[0].length/2)+posicionY && x >= (matriz.length/2 - palabra.length()/2)+((int)(center/2)*posicion) && x < (matriz.length/2 + palabra.length()/2 + palabra.length()%2)+((int)(center/2)*posicion)) {
-                    matriz[x][y] = palabra.charAt((x - matriz.length/2 + palabra.length() / 2)-((center/2)*posicion));
-                }
-            }
-        }
-        return matriz;
-    }
-
-    public char[][] cuadrito(int horizontal, int vertical){
-        char[][] matriz = new char[horizontal+1][vertical+1];
-        char borde;
-        char borde2;
-        for (int y = 0; y <= vertical; y++) {
-            for (int x = 0; x <= horizontal; x++) {
-                borde = '/';
-                borde2 = '|';
-                if(y==0 || y==vertical){
-                    matriz[x][y]=borde;
-                }else{
-                    if(x==0 || x==horizontal||x==1|| x==horizontal-1){
-                        matriz[x][y]=borde2;
-                    }else{
-                        matriz[x][y]=' ';
-                    }
-                }
-            }
-        }
-        return matriz;
-    }
-    //fin de funciones de cuadrito
 }
